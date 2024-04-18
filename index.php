fedca4adb4392efb58a6f16b0254ddb48e33f441
danielecolaci <danielecolaci@outlook.com>
2024-04-18 17:03:52 +0200

    Add Instances and Create 2 production objects

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 
-class Production{}
+class Production{
+    public $title;
+    public $language;
+    public $rating;
 
+    public function __construct($title, $language, $rating){
+        $this->title = $title;
+        $this->language = $language;
+        $this->rating = $rating;
+    }
+
+}
+
+$production1 = new Production("Avatar", "English", 8);
+$production2 = new Production("C'è ancora Domani", "Italian", 7);
+
+var_dump($production1);
+var_dump($production2);
 ?>
