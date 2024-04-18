b7c208b36d3e261e76fd9626d5f124a630de04da
danielecolaci <danielecolaci@outlook.com>
2024-04-18 17:10:53 +0200

    Print on screen the movies

--- index.php
+++ index.php
@@ -19,4 +19,8 @@ $production2 = new Production("C'è ancora Domani", "Italian", 7);
 var_dump($production1);
 var_dump($production2);
 
+echo '<div><h2>' . $production1->title . '</h2><p>' . $production1->language . '</p><h4>' . $production1->rating . '</h4></div>';
+echo '<div><h2>' . $production2->title . '</h2><p>' . $production2->language . '</p><h4>' . $production2->rating . '</h4></div>';
+
+
 ?>
